232ddb949bb6586dcc8d75b22b66f941b73a1368
Pedro Gomes <pedrogomes94@gmail.com>
2018-12-01 01:29:49 +0000

    remove dappName from callRequest

--- functions/index.js
+++ functions/index.js
@@ -28,12 +28,7 @@ exports.push = functions.https.onRequest(async function (req, res) {
     res.status(500).send('Error: sessionId is required')
   }
 
-  const dappName = req.body.dappName
-  if (!dappName) {
-    res.status(500).send('Error: dappName is required')
-  }
-
-  const body = 'New call request from ' + dappName
+  const body = 'New Request'
 
   admin
     .messaging()
